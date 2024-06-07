7738100b3fedb568110afb69b0315559500ae3d4
Tushar Balar <balar.tushar1@gmail.com>
2024-06-07 20:47:51 +0530

    Added animation to the image

--- components/intro.tsx
+++ components/intro.tsx
@@ -2,23 +2,38 @@
 
 import React from 'react'
 import Image from "next/image";
+import { motion } from 'framer-motion';
 
 export default function Intro() {
     return (
         <section
             id="home"
         >
-            <div>
-                <Image
-                    // src="https://images.unsplash.com/photo-1507003211169-0a1dd7228f2d?fit=crop&w=368&h=368&q=100"
-                    src="https://avatars.githubusercontent.com/u/4783557?v=4"
-                    alt="Tushar Balar"
-                    width="192"
-                    height="192"
-                    quality="95"
-                    priority={true}
-                    className="h-24 w-24 rounded-full object-cover border-[0.35rem] border-white shadow-xl"
-                />
+            <div className='flex items-center justify-center'>
+                <div className='relative'>
+                    <motion.div
+                        initial={{ opacity: 0, scale: 0 }}
+                        animate={{ opacity: 1, scale: 1 }}
+                        transition={{
+                            type: 'tween',
+                            duration: 0.2
+                        }}
+                    >
+                        <Image
+                            src="https://avatars.githubusercontent.com/u/4783557?v=4"
+                            alt="Tushar Balar"
+                            width="192"
+                            height="192"
+                            quality="95"
+                            priority={true}
+                            className="h-24 w-24 rounded-full object-cover border-[0.35rem] border-white shadow-xl"
+                        />
+                    </motion.div>
+
+                    <span className='absolute bottom-0 right-0 text-4xl'>
+                        👋
+                    </span>
+                </div>
             </div>
         </section>
     )
