ebaae8132d1df780e5ccf608aa0f447fb0c86f94
Tushar Balar <balar.tushar1@gmail.com>
2024-06-07 08:59:19 +0530

    Changed image

--- components/intro.tsx
+++ components/intro.tsx
@@ -10,8 +10,9 @@ export default function Intro() {
         >
             <div>
                 <Image
-                    src="https://images.unsplash.com/photo-1507003211169-0a1dd7228f2d?fit=crop&w=368&h=368&q=100"
-                    alt="Ricardo portrait"
+                    // src="https://images.unsplash.com/photo-1507003211169-0a1dd7228f2d?fit=crop&w=368&h=368&q=100"
+                    src="https://avatars.githubusercontent.com/u/4783557?v=4"
+                    alt="Tushar Balar"
                     width="192"
                     height="192"
                     quality="95"
