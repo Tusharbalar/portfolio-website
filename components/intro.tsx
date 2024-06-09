4ecda7818d1297578148486be4ea3cd24e63887a
Tushar Balar <balar.tushar1@gmail.com>
2024-06-09 07:31:15 +0530

    Fixed animation issue

--- components/intro.tsx
+++ components/intro.tsx
@@ -73,19 +73,19 @@ export default function Intro() {
             >
                 <Link
                     href="#contact"
-                    className="group bg-gray-900 text-white px-7 py-3 flex items-center gap-2 rounded-full outline-none focus:scale-110 hover:scale-110 hover:bg-gray-950 active:scale-105 transition"
+                    className="group bg-gray-900 text-white px-7 py-3 flex items-center gap-2 rounded-full outline-none focus:scale-105 hover:scale-105 hover:bg-gray-950 active:scale-105 transition"
                 >
                     Contact me here{" "}
                     <BsArrowRight className="opacity-70 group-hover:translate-x-1 transition" />
                 </Link>
 
                 <a
-                    className="group bg-white px-7 py-3 flex items-center gap-2 rounded-full outline-none focus:scale-110 hover:scale-110 active:scale-105 transition cursor-pointer borderBlack dark:bg-white/10"
+                    className="group bg-white px-7 py-3 flex items-center gap-2 rounded-full outline-none focus:scale-110 hover:scale-105 active:scale-103 transition cursor-pointer borderBlack dark:bg-white/10"
                     href="/CV.pdf"
                     download
                 >
                     Download CV{" "}
-                    <HiDownload className="opacity-60 group-hover:translate-y-1 transition" />
+                    <HiDownload className="opacity-60 group-hover:translate-x-1 transition" />
                 </a>
 
                 <a
