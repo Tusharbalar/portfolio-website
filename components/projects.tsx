55ff229e08639544b265e6105b218a36cd70c7ee
Tushar Balar <balar.tushar1@gmail.com>
2024-06-11 22:22:39 +0530

    Added skills

--- components/projects.tsx
+++ components/projects.tsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import SectionHeading from './section-heading'
 import { projectsData } from '@/lib/data'
+import Image from 'next/image';
 
 export default function Projects() {
   return (
@@ -27,5 +28,14 @@ function Project({
   tags,
   imageUrl
 }: ProjectProp) {
-  return <div>{title}</div>
+  return <section className='bg-gray-100 max-w-[42rem] border border-black/5 overflow-hidden sm:pr-8'>
+    <h3 className='text-2xl font-semibold'>{title}</h3>
+    <p className='mt-2 leading-relaxed text-gray-700'>{description}</p>
+    <ul className='flex flex-wrap mt-4 gap-2'>
+      {tags.map((tag, index) => (
+        <li className='bg-black px-3 py-1 text-[0.7rem] uppercase tracking-wider text-white rounded-full' key={index}>{tag}</li>
+      ))}
+    </ul>
+    <Image src={imageUrl} alt="Project I worked on" quality={95} />
+  </section>
 }
