34e77f3db93a9a3bf8c4009a23c014d9b7cefc47
Tushar Balar <balar.tushar1@gmail.com>
2024-06-11 22:56:02 +0530

    Added animation in the project

--- components/projects.tsx
+++ components/projects.tsx
@@ -28,7 +28,7 @@ function Project({
   tags,
   imageUrl
 }: ProjectProp) {
-  return <section className='group bg-gray-100 max-w-[42rem] border border-black/5 overflow-hidden sm:pr-8 relative sm:h-[20rem] mb-3 sm:mb-8 last:mb-0 even:pl-8'>
+  return <section className='group bg-gray-100 max-w-[42rem] border border-black/5 overflow-hidden sm:pr-8 relative sm:h-[20rem] mb-3 sm:mb-8 last:mb-0 even:pl-8 hover:bg-gray-200 transition'>
     <div className='py-4 pb-7 px-5 sm:pl-10 sm:pr-2 sm:pt-10 sm:max-w-[50%] flex flex-col h-full group-even:ml-[18rem]'>
       <h3 className='text-2xl font-semibold'>{title}</h3>
       <p className='mt-2 leading-relaxed text-gray-700'>{description}</p>
@@ -42,7 +42,7 @@ function Project({
       src={imageUrl}
       alt="Project I worked on"
       quality={95}
-      className='absolute top-8 -right-40 w-[28.25rem] rounded-t-lg shadow-2xl group-even:right-[initial] group-even:-left-40'
+      className='absolute top-8 -right-40 w-[28.25rem] rounded-t-lg shadow-2xl group-even:right-[initial] group-even:-left-40 transition group-hover:-translate-x-3 group-hover:translate-y-3 group-hover:-rotate-2 group-even:group-hover:translate-x-3 group-even:group-hover:translate-y-3 group-even:group-hover:rotate-2 group-hover:scale-[1.04]'
     />
   </section>
 }
