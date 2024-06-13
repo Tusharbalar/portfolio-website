740c4049de8cc642dfe8c577d577d1c43ccd42cb
Tushar Balar <balar.tushar1@gmail.com>
2024-06-13 21:39:25 +0530

    Fix the styling issue

--- components/project.tsx
+++ components/project.tsx
@@ -21,22 +21,20 @@ export default function Project({
     target: ref,
     offset: ["0 1", "1.33 1"]
   })
-  const scaleProgress = useTransform(scrollYProgress, [0, 1], [0.5, 1])
-  const opacityProgress = useTransform(scrollYProgress, [0, 1], [0.6, 1])
+  const scaleProgress = useTransform(scrollYProgress, [0, 1], [0.7, 1])
+  const opacityProgress = useTransform(scrollYProgress, [0, 1], [0.7, 1])
 
   return (
-
     <motion.div
       style={{
         scale: scaleProgress,
         opacity: opacityProgress
       }}
-      className='mb-3 sm:mb-8 last:mb-0'
+      className='group mb-3 sm:mb-8 last:mb-0'
     >
       <section
         ref={ref}
-
-        className='group bg-gray-100 max-w-[42rem] border border-black/5 overflow-hidden sm:pr-8 relative sm:h-[20rem] mb-3 sm:mb-8 last:mb-0 even:pl-8 hover:bg-gray-200 transition'>
+        className='bg-gray-100 max-w-[42rem] border rounded-lg border-black/5 overflow-hidden sm:pr-8 relative sm:h-[20rem] mb-3 sm:mb-8 last:mb-0 group-even:pl-8 hover:bg-gray-200 transition'>
         <div className='py-4 pb-7 px-5 sm:pl-10 sm:pr-2 sm:pt-10 sm:max-w-[50%] flex flex-col h-full group-even:ml-[18rem]'>
           <h3 className='text-2xl font-semibold'>
             <Link href={githubUrl} target='_blank'>
