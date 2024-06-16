49239b5af4214a0809368e1c28e6866f2f96b829
Tushar Balar <balar.tushar1@gmail.com>
2024-06-16 22:39:59 +0530

    Created Skill component

---
+++ components/skills.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import SectionHeading from './section-heading'
+
+export default function Skils() {
+  return (
+    <section>
+      <SectionHeading>
+        My Skills
+      </SectionHeading>
+    </section>
+  )
+}
