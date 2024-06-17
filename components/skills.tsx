20ca708cd56c8339de2366f83d013307fa7d4aa1
Tushar Balar <balar.tushar1@gmail.com>
2024-06-17 22:43:17 +0530

    Added skills data

--- components/skills.tsx
+++ components/skills.tsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import SectionHeading from './section-heading'
+import { skillsData } from '@/lib/data'
 
 export default function Skils() {
   return (
@@ -7,6 +8,13 @@ export default function Skils() {
       <SectionHeading>
         My Skills
       </SectionHeading>
+      <ul>
+        {
+          skillsData.map((skill, index) => (
+            <li key={index}>skill</li>
+          ))
+        }
+      </ul>
     </section>
   )
 }
