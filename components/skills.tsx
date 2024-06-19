5e1c42ccd1427d8cb85ad3249006121e5d572329
Tushar Balar <balar.tushar1@gmail.com>
2024-06-19 22:10:09 +0530

    Fixed Skills component issue and added animation

--- components/skills.tsx
+++ components/skills.tsx
@@ -1,17 +1,51 @@
+'use client';
+
 import React from 'react'
 import SectionHeading from './section-heading'
 import { skillsData } from '@/lib/data'
+import useSectionInView from '@/lib/hooks';
+import { animate, motion } from 'framer-motion';
 
+const fadeInAnimationsVariants = {
+  initial: {
+    opacity: 0,
+    y: 100
+  },
+  animate: (index: number) => ({
+    opacity: 1,
+    y: 0,
+    transition: {
+      delay: 0.05 * index
+    }
+  })
+}
 export default function Skils() {
+  const { ref } = useSectionInView('Skills', 0.5);
+
   return (
-    <section className='mb-28 max-w-[53rem] scroll-mt-28 text-center sm:mb-40'>
+    <section
+      id="skills"
+      ref={ref}
+      className='mb-28 max-w-[53rem] scroll-mt-28 text-center sm:mb-40'>
       <SectionHeading>
         My Skills
       </SectionHeading>
       <ul className='flex flex-wrap justify-center gap-2 text-lg text-gray-800'>
         {
           skillsData.map((skill, index) => (
-            <li className='bg-white border border-blacl-[0.1] rounded-xl px-5 py-3' key={index}>{skill}</li>
+            <motion.li
+              className='bg-white border border-blacl-[0.1] rounded-xl px-5 py-3'
+              key={index}
+              variants={fadeInAnimationsVariants}
+              initial="initial"
+              whileInView="animate"
+              viewport={{
+                once: true
+              }}
+              custom={index}
+            >
+              {skill}
+            </motion.li>
           ))
         }
       </ul>
