194e26e3a649665406d19b7afe344a995957b22e
Tushar Balar <balar.tushar1@gmail.com>
2024-06-27 22:53:34 +0530

    Implimented ThemeSwitch Component

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,6 +4,8 @@ import "./globals.css";
 import Header from "@/components/header";
 import ActiveSectionContextProvider from "@/context/active-section-context";
 import { Toaster } from "react-hot-toast";
+import Footer from "@/components/footer";
+import ThemeSwitch from "@/components/theme-switch";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -29,6 +31,7 @@ export default function RootLayout({
           <Footer />
           <Toaster position="top-right" />
         </ActiveSectionContextProvider>
+        <ThemeSwitch />
       </body>
     </html>
   );
