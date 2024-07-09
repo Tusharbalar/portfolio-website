bc8d206ecd0797dcee2609929007384af9324a12
Tushar Balar <balar.tushar1@gmail.com>
2024-07-09 22:35:06 +0530

    Updated contact component

--- components/contact.tsx
+++ components/contact.tsx
@@ -1,10 +1,11 @@
-'use client';
+"use client";
 
-import React from 'react'
-import SectionHeading from './section-heading'
-import useSectionInView from '@/lib/hooks';
-import { motion } from 'framer-motion';
-import { sendEmail } from '@/actions/sendEmail';
+import React from "react";
+import SectionHeading from "./section-heading";
+import { motion } from "framer-motion";
+import useSectionInView from "@/lib/hooks";
+import { sendEmail } from "@/actions/sendEmail";
+import SubmitBtn from "./submit-btn";
 import toast from "react-hot-toast";
 
 export default function Contact() {
@@ -14,18 +15,18 @@ export default function Contact() {
     <motion.section
       id="contact"
       ref={ref}
-      className='mb-20 sm:mb-28 w-[min-(100%,38rem)] text-center'
+      className="mb-20 sm:mb-28 w-[min(100%,38rem)] text-center"
       initial={{
-        opacity: 0
+        opacity: 0,
       }}
       whileInView={{
-        opacity: 1
+        opacity: 1,
       }}
       transition={{
-        duration: 1
+        duration: 1,
       }}
       viewport={{
-        once: true
+        once: true,
       }}
     >
       <SectionHeading>Contact me</SectionHeading>
@@ -37,34 +38,37 @@ export default function Contact() {
         </a>{" "}
         or through this form.
       </p>
+
       <form
         className="mt-10 flex flex-col dark:text-black"
         action={async (formData) => {
           const { data, error } = await sendEmail(formData);
+
           if (error) {
             toast.error(error);
             return;
           }
 
           toast.success("Email sent successfully!");
-        }}>
+        }}
+      >
         <input
-          type='email'
-          name="senderEmail"
           className="h-14 px-4 rounded-lg borderBlack dark:bg-white dark:bg-opacity-80 dark:focus:bg-opacity-100 transition-all dark:outline-none"
-          placeholder='Your email'
+          name="senderEmail"
+          type="email"
           required
           maxLength={500}
+          placeholder="Your email"
         />
         <textarea
           className="h-52 my-3 rounded-lg borderBlack p-4 dark:bg-white dark:bg-opacity-80 dark:focus:bg-opacity-100 transition-all dark:outline-none"
           name="message"
-          placeholder='Your message'
+          placeholder="Your message"
           required
           maxLength={5000}
         />
         <SubmitBtn />
       </form>
     </motion.section>
-  )
+  );
 }
