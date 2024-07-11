dedfcf5a8db6e1315ddb36366d22edf75bd19511
Tushar Balar <balar.tushar1@gmail.com>
2024-07-11 22:13:48 +0530

    Use useTheme hook in theme switch component

--- components/theme-switch.tsx
+++ components/theme-switch.tsx
@@ -1,5 +1,6 @@
 "use client";
 
+import { useTheme } from '@/context/theme-context';
 import React, { useEffect, useState } from 'react'
 import { BsMoon, BsSun } from 'react-icons/bs'
 
@@ -7,6 +8,7 @@ type Theme = "light" | "dark";
 
 export default function ThemeSwitch() {
   const [theme, setTheme] = useState<Theme>("light");
+  const { toggleTheme } = useTheme();
 
   useEffect(() => {
     const localTheme = window.localStorage.getItem("theme") as Theme | null;
@@ -23,18 +25,6 @@ export default function ThemeSwitch() {
       window.localStorage.setItem("theme", "dark");
     }
   }, []);
-
-  const toggleTheme = () => {
-    if (theme === 'light') {
-      setTheme('dark');
-      window.localStorage.setItem("theme", "dark");
-      document.documentElement.classList.add("dark");
-    } else {
-      setTheme("light");
-      window.localStorage.setItem("theme", "light");
-      document.documentElement.classList.remove("dark");
-    }
-  }
 
   return (
     <button className="fixed bottom-5 right-5 bg-white w-[3rem] h-[3rem] bg-opacity-80 backdrop-blur-[0.5rem] border border-white border-opacity-40 shadow-2xl rounded-full flex items-center justify-center hover:scale-[1.15] active:scale-105 transition-all dark:bg-gray-950" onClick={toggleTheme}>
